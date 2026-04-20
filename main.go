a69e81f6b27bfe973f7d13ca4a0eb2b76184a562
Kien <kienlt@abigames.com.vn>
2026-04-20 08:26:10 +0700

    Push

--- main.go
+++ main.go
@@ -2,7 +2,19 @@ package main
 
 import "fmt"
 
+const CONST_ITEM = "DatLzz"
+
 func main() {
-	fmt.Println("Hello World")
-	randomUser()
+	// phone := "123-456-7890"
+	// fmt.Println(phone)
+	// item = "This is an item"
+	// fmt.Println(item)
+	// var hoten string
+	// fmt.Print("Nhập họ tên: ")
+	// scaner := bufio.NewScanner(os.Stdin)
+	// if scaner.Scan() {
+	// 	hoten = scaner.Text()
+	// }
+	// fmt.Println("Họ tên bạn đã nhập:", hoten)
+	fmt.Println("Gia tri ham so:" + CONST_ITEM)
 }
