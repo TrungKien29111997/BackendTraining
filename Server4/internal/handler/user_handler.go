40798a53cb0a6da2c8a48ea884636d0bea73698c
Kien <kienlt@abigames.com.vn>
2026-07-23 08:29:19 +0700

    Update DTO

--- Server4/internal/handler/user_handler.go
+++ Server4/internal/handler/user_handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"net/http"
+	"sever4/internal/dto"
 	"sever4/internal/models"
 	"sever4/internal/service"
 	"sever4/internal/utils"
@@ -25,13 +26,16 @@ func (uh *UserHandler) CreateUser(c *gin.Context) {
 	var user models.User
 	if err := c.ShouldBindJSON(&user); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
 	}
 	createdUser, err := uh.service.CreateUser(user)
 	if err != nil {
 		utils.ResponeError(c, err)
 		return
 	}
-	utils.ReponseSuccess(c, http.StatusCreated, createdUser)
+
+	userDTO := dto.MapUserToDTO(createdUser)
+	utils.ReponseSuccess(c, http.StatusCreated, &userDTO)
 }
 func (uh *UserHandler) GetUserByUUID(c *gin.Context) {
 
