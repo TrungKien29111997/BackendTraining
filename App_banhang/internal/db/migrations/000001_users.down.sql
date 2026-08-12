b5d5649c3967bbc2691e2403acc89ce6a0703137
Kien <kienlt@abigames.com.vn>
2026-08-12 08:25:41 +0700

    SQL tao bang user

--- App_banhang/internal/db/migrations/000001_users.down.sql
+++ App_banhang/internal/db/migrations/000001_users.down.sql
@@ -1,1 +1,15 @@
-drop table if exists users;
+-- Drop trigger if exists
+DROP TRIGGER IF EXISTS set_user_updated_at ON users;
+
+-- Drop function if exists
+DROP FUNCTION IF EXISTS update_user_updated_at_column;
+
+-- Drop index if exists
+DROP INDEX IF EXISTS user_email_status_idx;
+DROP INDEX IF EXISTS user_level_idx;
+DROP INDEX IF EXISTS user_status_idx;
+DROP INDEX IF EXISTS user_created_at_idx;
+DROP INDEX IF EXISTS user_deleted_at_idx;
+
+-- Drop table
+DROP TABLE IF EXISTS users;
